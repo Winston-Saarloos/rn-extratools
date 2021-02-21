19271e754a511461a83d0e9f47372c929cceff41
97rocko <97rocko@gmail.com>
2021-02-21 00:43:44 -0600

    created photo grid display component

--- src/components/ImageBrowserHeader.js
+++ src/components/ImageBrowserHeader.js
@@ -12,13 +12,16 @@ function ImageBrowserHeader() {
         <nav className="navbar navbar-expand navbar-dark bg-dark">
             <a className="navbar-brand" id="lblVersionNumber" href={url}>Version W.I.P</a>
             <div id="lblImageResultNumber">Image Results: 0</div>
-            <button type="button" className="btn btn-secondary" onClick={EmptyFunction} id="btnFeedLibrary" value="0">User Photo Feed</button>
-            <button type="button" className="btn btn-secondary oldestNewest" onClick={EmptyFunction} id="btnOldestToNewest" value="0">Newest to Oldest</button>
+          <button type="button" className="btn btn-secondary" onClick={EmptyFunction} id="btnFeedLibrary" value="0">User Photo Feed</button>
+          <button type="button" className="btn btn-secondary oldestNewest" onClick={EmptyFunction} id="btnOldestToNewest" value="0">Newest to Oldest</button>
 
-            <form className="form-inline my-2 my-lg-0">
-                <input className="form-control mr-sm-2" type="text" placeholder="RR username" aria-label="@ RecNet Username" id="txtUsername" />
-                <button className="btn btn-success my-2 my-sm-0" id="btnLoad" onClick={EmptyFunction}>Load Images</button>
-            </form>
+          <div className="input-group w-25 mr-2">
+            <div className="input-group-prepend">
+              <span className="input-group-text" id="basic-addon1">@</span>
+            </div>
+            <input type="text" className="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1" />
+          </div>
+          <button className="btn btn-success my-2 my-sm-0" id="btnLoad" onClick={EmptyFunction}>Load Images</button>
         </nav>
       </div>
     );
--- src/components/ImageBrowserMain.js
+++ src/components/ImageBrowserMain.js
@@ -1,9 +1,43 @@
-function ImageBrowserMain() {
+import React, { Component } from 'react';
+import './ImageBrowserMain.css';
+
+function EmptyFunction() {
+  console.log('This was an empty function.');
+}
+
+class ImageBrowserMain extends Component {
+  
+  render() {
     return (
       <div className="ImageBrowserMain">
-  
+        <div id="photoGrid">
+          <div className="grid col-12" id="grid">
+            <div className="grid-item" type="button" data-toggle="modal" data-target="#imageDetailModal" onClick={EmptyFunction}>
+              <img alt="Image1" data-src="https://img.rec.net/4214c04942d846bbb4220e248650fcd0.jpg?width=500" className="grid-image" src="https://img.rec.net/4214c04942d846bbb4220e248650fcd0.jpg?width=500" />
+            </div>
+            <div className="grid-item" type="button" data-toggle="modal" data-target="#imageDetailModal" onClick={EmptyFunction}>
+              <img alt="Image1" data-src="https://img.rec.net/4214c04942d846bbb4220e248650fcd0.jpg?width=500" className="grid-image" src="https://img.rec.net/4214c04942d846bbb4220e248650fcd0.jpg?width=500" />
+            </div>
+            <div className="grid-item" type="button" data-toggle="modal" data-target="#imageDetailModal" onClick={EmptyFunction}>
+              <img alt="Image1" data-src="https://img.rec.net/4214c04942d846bbb4220e248650fcd0.jpg?width=500" className="grid-image" src="https://img.rec.net/4214c04942d846bbb4220e248650fcd0.jpg?width=500" />
+            </div>
+            <div className="grid-item" type="button" data-toggle="modal" data-target="#imageDetailModal" onClick={EmptyFunction}>
+              <img alt="Image1" data-src="https://img.rec.net/4214c04942d846bbb4220e248650fcd0.jpg?width=500" className="grid-image" src="https://img.rec.net/4214c04942d846bbb4220e248650fcd0.jpg?width=500" />
+            </div>
+            <div className="grid-item" type="button" data-toggle="modal" data-target="#imageDetailModal" onClick={EmptyFunction}>
+              <img alt="Image1" data-src="https://img.rec.net/4214c04942d846bbb4220e248650fcd0.jpg?width=500" className="grid-image" src="https://img.rec.net/4214c04942d846bbb4220e248650fcd0.jpg?width=500" />
+            </div>
+            <div className="grid-item" type="button" data-toggle="modal" data-target="#imageDetailModal" onClick={EmptyFunction}>
+              <img alt="Image1" data-src="https://img.rec.net/4214c04942d846bbb4220e248650fcd0.jpg?width=500" className="grid-image" src="https://img.rec.net/4214c04942d846bbb4220e248650fcd0.jpg?width=500" />
+            </div>
+            <div className="grid-item" type="button" data-toggle="modal" data-target="#imageDetailModal" onClick={EmptyFunction}>
+              <img alt="Image1" data-src="https://img.rec.net/4214c04942d846bbb4220e248650fcd0.jpg?width=500" className="grid-image" src="https://img.rec.net/4214c04942d846bbb4220e248650fcd0.jpg?width=500" />
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
-  
-  export default ImageBrowserMain;
+}
+
+export default ImageBrowserMain;
